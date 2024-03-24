157752b25f4fa2c96aa97080498e78a6fb635edc
Usman <it.dev45@dawaateislami.net>
2024-03-24 15:39:39 +0500

    fix product question page issue

--- app/Http/Controllers/web/WebController.php
+++ app/Http/Controllers/web/WebController.php
@@ -137,6 +137,12 @@ class WebController extends Controller
 
 
     // cloned methods of myweightloss
+    public function product_question_new()
+    {
+        $data['user'] = auth()->user() ?? [];
+        return view('web.pages.product_question', $data);
+    }
+
     public function products(Request $request)
     {
         session()->forget('pro_id');

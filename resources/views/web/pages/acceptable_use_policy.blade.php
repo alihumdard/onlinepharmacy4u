dac09af4cf2ba315efa4fdb4e90b18edf5fe0bcc
sanader <sanaderali007@gmail.com>
2024-03-31 05:15:16 +0500

    double check

--- resources/views/web/pages/acceptable_use_policy.blade.php
+++ resources/views/web/pages/acceptable_use_policy.blade.php
@@ -10,16 +10,16 @@
 <div class="ltn__utilize-overlay"></div>
 
 <!-- BREADCRUMB AREA START -->
-<div class="ltn__breadcrumb-area text-left bg-image " data-bs-bg="img/Pharmacy4banners/help.webp">
+<div class="ltn__breadcrumb-area text-left bg-image " data-bs-bg="img/banner/AcceptableUsePolicy.png">
     <div class="container">
         <div class="row">
             <div class="col-lg-12">
                 <div class="ltn__breadcrumb-inner">
-                    <h1 class="page-title mt-5 text-white"> Acceptable Use Policy</h1>
+                    <h1 class="page-title mt-5 text-black"> Acceptable Use Policy</h1>
                     <div class="ltn__breadcrumb-list">
                         <ul>
-                            <li><a href="/" class="text-white"><span class="ltn__secondary-color"><i class="fas fa-home"></i></span> Home</a></li>
-                            <li class="text-white"> Acceptable Use Policy</li>
+                            <li><a href="/" class="text-black"><span class="ltn__secondary-color"><i class="fas fa-home"></i></span> Home</a></li>
+                            <li class="text-black"> Acceptable Use Policy</li>
                         </ul>
                     </div>
                 </div>
@@ -28,51 +28,7 @@
     </div>
 </div>
 <!-- BREADCRUMB AREA END -->
-<div class="container pt-0 mt-0">
-    <div class="row">
-        <div class="col-md-3"></div>
-        <div class="col-md-6">
-            <h1 class="text-center">Policies</h1>
-            <p class="text-center">Here you can find all documents relating to our policies</p>
-        </div>
-        <div class="col-md-3"></div>
-    </div>
-    <div class="container pb-5">
-        <div class="row">
-            <div class="col-md-3 pt-2">
-                <a href="#" class="btn  policy-btn">Opioid Policy</a>
-            </div>
-            <div class="col-md-3 pt-2">
-                <a href="#" class="btn  policy-btn">Privacy & Cookies Policy</a>
-            </div>
-            <div class="col-md-3 pt-2">
-                <a href="#" class="btn  policy-btn">Acceptable Use Policy</a>
-            </div>
-            <div class="col-md-3 pt-2">
-                <a href="#" class="btn  policy-btn">Terms & Conditions</a>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-md-3 pt-2">
-                <a href="/faq" class="btn  policy-btn">Faq</a>
-            </div>
-            <div class="col-md-3 pt-2">
-                <a href="#" class="btn  policy-btn">Editorial Policy</a>
-            </div>
-            <div class="col-md-3 pt-2">
-                <a href="/returns" class="btn  policy-btn">Returns Policy</a>
-            </div>
-            <div class="col-md-3 pt-2">
-                <a href="#" class="btn  policy-btn">Dispensing Frequencies</a>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-md-3 pt-2">
-                <a href="#" class="btn  policy-btn">Identity Verification</a>
-            </div>
-        </div>
-    </div>
-</div>
+
 
 
 

ca34f03c4a9af28b0d820ff14bb6b2ddaa31d264
alihumdard <alihumdard125@gmail.com>
2024-04-03 01:34:13 +0500

    updations in the alert

---
+++ app/Models/Alert.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Alert extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'type',
+        'body',
+        'route',
+        'slug',
+        'question_id',
+        'q_category_id',
+        'user_id',
+        'status',
+        'created_by',
+        'updated_by',
+    ];
+}
---
+++ database/migrations/2024_04_02_095926_create_alerts_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('alerts', function (Blueprint $table) {
+            $table->id();
+            $table->string('type');
+            $table->text('body');
+            $table->string('route');
+            $table->string('slug')->nullable();
+            $table->unsignedBigInteger('question_id')->nullable();
+            $table->unsignedBigInteger('option')->nullable();
+            $table->unsignedBigInteger('q_category_id')->nullable();
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->string('status')->default('Active');
+            $table->unsignedBigInteger('created_by')->nullable();
+            $table->unsignedBigInteger('updated_by')->nullable()->default(null);
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('alerts');
+    }
+};

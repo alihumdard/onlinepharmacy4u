ec65fc1c46b855853da465230c11a063b79d0961
Ahtasham22 <146159780+Ahtasham22@users.noreply.github.com>
2024-04-16 00:29:11 +0500

    adding css set menu set login

--- resources/views/web/pages/registration_form.blade.php
+++ resources/views/web/pages/registration_form.blade.php
@@ -20,7 +20,7 @@
 </style>
 
 <!-- BREADCRUMB AREA START -->
-<div class="ltn__breadcrumb-area text-left bg-overlay-white-30 bg-image "  data-bs-bg="img/allbanners/signin.webp">
+<!-- <div class="ltn__breadcrumb-area text-left bg-overlay-white-30 bg-image "  data-bs-bg="img/allbanners/signin.webp">
     <div class="container">
         <div class="row">
             <div class="col-lg-12">
@@ -36,18 +36,18 @@
             </div>
         </div>
     </div>
-</div>
+</div> -->
 <!-- BREADCRUMB AREA END -->
 
 <!-- LOGIN AREA START (Register) -->
-<div class="ltn__login-area pb-110">
+<div class="ltn__login-area pb-110 py-5">
     <div class="container">
         <div class="row">
             <div class="col-lg-12">
                 <div class="section-title-area text-center">
                     <h1 class="section-title">Register <br>Your Account</h1>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. <br>
-                         Sit aliquid,  Non distinctio vel iste.</p>
+                    <!-- <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. <br>
+                         Sit aliquid,  Non distinctio vel iste.</p> -->
                 </div>
             </div>
         </div>
@@ -108,15 +108,15 @@
                         <div class="alert-danger text-danger ">{{ $message }}</div>
                         @enderror
                         
-                        <div class="btn-wrapper">
-                            <button class="theme-btn-1 btn reverse-color btn-block" type="submit">CREATE ACCOUNT</button>
+                        <div class="btn-wrapper text-center px-3">
+                            <button class="theme-btn-1 btn reverse-color btn-block text-center px-3" type="submit">CREATE ACCOUNT</button>
                         </div>
                     </form>
                     <div class="by-agree text-center">
                         <p>By creating an account, you agree to our:</p>
                         <p><a href="/terms_and_conditions/">TERMS OF CONDITIONS  &nbsp; &nbsp; | &nbsp; &nbsp;  PRIVACY POLICY</a></p>
                         <div class="go-to-btn mt-50">
-                            <a href="/admin">ALREADY HAVE AN ACCOUNT ?</a>
+                            <a href="/admin"><strong>ALREADY HAVE AN ACCOUNT?</strong></a>
                         </div>
                     </div>
                 </div>

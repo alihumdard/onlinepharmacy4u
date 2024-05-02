0a5fd9e231e6116cfa1b338efc777094045d2eff
alihumdard <132681271+alihumdard@users.noreply.github.com>
2024-05-02 19:05:12 +0500

    updation in the edit

--- resources/views/admin/pages/categories/child_categories.blade.php
+++ resources/views/admin/pages/categories/child_categories.blade.php
@@ -169,7 +169,7 @@
         }).buttons().container();
     });
     $(document).ready(function() {
-        $('.edit').click(function() {
+        $(document).on('click', '.edit',function() {
             var id = $(this).data('id');
             $('#edit_form_id_input').val(id);
             $('#edit_form').submit();

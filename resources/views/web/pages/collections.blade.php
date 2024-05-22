27fb77e9c7be1691ffc3e2fde984e1d911d4c760
Ahtasham22 <146159780+Ahtasham22@users.noreply.github.com>
2024-05-23 00:59:37 +0500

    sub category

--- resources/views/web/pages/collections.blade.php
+++ resources/views/web/pages/collections.blade.php
@@ -13,11 +13,17 @@
     }
 </style>
 <!-- BREADCRUMB AREA START -->
-<div class="ltn__breadcrumb-area text-left bg-overlay-white-30 bg-image "  data-bs-bg="{{ asset('storage/'.$image) }}">
+<!-- data-bs-bg="{{ asset('storage/'.$image) }}" -->
+<div class="ltn__breadcrumb-area text-left bg-overlay-white-30 bg-image " >
     <div class="container">
         <div class="row">
-            <div class="col-lg-12">
-                <div class="ltn__breadcrumb-inner">
+            <div class="col-md-6">
+                <div class="product-same-design">
+                    <img src="{{ asset('storage/'.$image) }}" alt="">
+                </div>
+            </div>
+            <div class="col-md-6">
+            <div class="ltn__breadcrumb-inner">
                     <div class="ltn__breadcrumb-list">
                         <ul>
                             <li><a href="index.html"><span class="ltn__secondary-color"><i class="fas fa-home"></i></span> Home</a></li>
@@ -27,6 +33,17 @@
                     <h1 class="page-title">{{ $category_name}}</h1>
                 </div>
             </div>
+            <!-- <div class="col-lg-12">
+                <div class="ltn__breadcrumb-inner">
+                    <div class="ltn__breadcrumb-list">
+                        <ul>
+                            <li><a href="index.html"><span class="ltn__secondary-color"><i class="fas fa-home"></i></span> Home</a></li>
+                            <li>{{ $category_name}}</li>
+                        </ul>
+                    </div>
+                    <h1 class="page-title">{{ $category_name}}</h1>
+                </div>
+            </div> -->
         </div>
     </div>
 </div>

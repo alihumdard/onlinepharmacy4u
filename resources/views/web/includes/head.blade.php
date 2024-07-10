5aa235a7145a5eec27b79c26f81ac74e0eb6c271
alihumdard <alihumdard125@gmail.com>
2024-07-10 17:06:47 +0500

    updation in the live changes

--- resources/views/web/includes/head.blade.php
+++ resources/views/web/includes/head.blade.php
@@ -7,18 +7,33 @@
 <!-- Place favicon.png in the root directory -->
 <link rel="shortcut icon" href="/public/assets/web/images/icons/logo_32x32.webp" type="image/x-icon" />
 <!-- Font Icons css -->
-{{-- <link rel="stylesheet" href="css/font-icons.css"> --}}
 <link rel="stylesheet" href="{{ asset('/css/font-icons.css') }}">
 <!-- plugins css -->
-{{-- <link rel="stylesheet" href="css/plugins.css"> --}}
 <link rel="stylesheet" href="{{ asset('/css/plugins.css') }}">
 <!-- Main Stylesheet -->
-{{-- <link rel="stylesheet" href="css/style.css"> --}}
 <link rel="stylesheet" href="{{ asset('/css/style.css') }}">
 <!-- Responsive css -->
-{{-- <link rel="stylesheet" href="css/responsive.css"> --}}
 <link rel="stylesheet" href="{{ asset('/css/responsive.css') }}">
 <!-- Custom stylesheet for developer -->
-{{-- <link rel="stylesheet" href="css/custom.css"> --}}
 <link rel="stylesheet" href="{{ asset('/css/custom.css') }}">
 <meta name="csrf-token" content="{{ csrf_token() }}">
+
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-YLTKG6M9Z7"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-YLTKG6M9Z7');
+</script>
+
+
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-T99GH5S3');</script>
+<!-- End Google Tag Manager -->
